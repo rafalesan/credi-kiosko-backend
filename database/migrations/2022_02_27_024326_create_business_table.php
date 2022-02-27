e0ae7d5f1255cbf92951ce93ca0c2e5884dfad0f
Rafael Antonio Alegría Sánchez <rafael.alegria@golabstech.com>
2022-02-26 21:27:07 -0600

    Add migration to create business table

---
+++ database/migrations/2022_02_27_024326_create_business_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('business', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('name', 100);
+            $table->string('email', 150)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('business');
+    }
+};
