832f321b2b81aa67fd16f6a1db2ee0a23729e4cd
Rafael Antonio Alegría Sánchez <rafael.alegria@golabstech.com>
2022-02-27 01:07:00 -0600

    Create login endpoint to authenticate business users

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -38,6 +39,33 @@ class AuthController extends Controller
 
         return response()->json([
             'message' => 'User Registered',
+            'data' => [
+                'token' => $token->plainTextToken,
+                'user' => $user,
+            ],
+        ]);
+
+    }
+
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'The provided credentials are incorrect.',
+            ], 401);
+        }
+
+        $token = $user->createToken($request->device_name);
+
+        return response()->json([
+            'message' => 'User Authenticated.',
             'data' => [
                 'token' => $token->plainTextToken,
                 'user' => $user,
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('businesses/register', [AuthController::class, 'register']);
+Route::post('businesses/login', [AuthController::class, 'login']);
