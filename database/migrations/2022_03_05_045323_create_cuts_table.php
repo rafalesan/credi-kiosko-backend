a13952baae3bbf1830b51e42d5931d096f405780
Rafael Antonio Alegría Sánchez <rafael.alegria@golabstech.com>
2022-03-04 23:11:36 -0600

    Create table migration for cut-offs

---
+++ database/migrations/2022_03_05_045323_create_cuts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cuts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->timestamp('cut_off_date');
+            $table->decimal('total_credits', 17, 4);
+            $table->decimal('total_payments', 17, 4);
+            $table->decimal('total_arrears', 17, 4);
+            $table->decimal('balance', 17, 4);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cuts');
+    }
+};
